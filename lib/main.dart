3adec517620f5a068170269013ba0615550bc8a8
issacclicker <0712wn@gmail.com>
2025-07-12 03:56:27 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,14 @@ import 'signup_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyBMAA8FQWDKJUPQOtS3-2SQuJGEZ4Emt-0",
+      appId: "1:701252937477:android:5ec4cd697b51af9c0f396c",
+      messagingSenderId: "701252937477",
+      projectId: "sogongtest-c066f",
+    )
+  );
   runApp(const MyApp());
 }
 
