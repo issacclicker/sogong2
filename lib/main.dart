393d9fb2af260e8040f3f038523518dde6c3152f
issacclicker <0712wn@gmail.com>
2025-07-13 22:42:43 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,7 @@ import 'firebase_options.dart'; // firebase_options.dart 임포트
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: FirebaseOptions(
-        apiKey: "AIzaSyBMAA8FQWDKJUPQOtS3-2SQuJGEZ4Emt-0",
-        appId: "1:701252937477:android:5ec4cd697b51af9c0f396c",
-        messagingSenderId: "701252937477",
-        projectId: "sogongtest-c066f",
-      )
+      options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
